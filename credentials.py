a972d16df793050912abb39dc73257aeea2de1e4
Imantha <imantha@yahoo.com>
2020-10-15 13:46:19 +0800

    Create credentials.py

---
+++ credentials.py
@@ -0,0 +1,14 @@
+# Credentials for Twitter API
+# __________________________________________________________________________________________
+
+# Replace the ??? with your api key, api secret, consumer key & consumer secret
+api_key = '???'
+api_secret_key = '???'
+access_token = '???'
+access_token_secret = '???'
+
+# Mapbox Credentials
+# _________________________________________________________________________________________
+
+# Replace the ?? with your mapbox key
+mapbox_key = "???"
